71a8507aff73751dafc98241ae23aeaf5fde5387
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-20 10:14:41 +0300

    Done

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 100);
+Console.WriteLine($"[{string.Join(", ", array)}]");
