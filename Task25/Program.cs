542a84b2c281f81c680abc27340698c700dd9b8b
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-20 10:11:04 +0300

    Done

---
+++ Task25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число: ");
+int m = int.Parse(Console.ReadLine()!);
+for (int i = 2; i < m; i++)
+    m = m * i;
+    Console.WriteLine($"{n}");
